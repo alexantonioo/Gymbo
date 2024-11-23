7fdb413d3eb5522bd0e1cf8877f23822d238dfa3
gitsusdav <114825973+gitsusdav@users.noreply.github.com>
2024-11-23 01:17:49 -0400

    ronnie added
    login

--- core/urls.py
+++ core/urls.py
@@ -1,15 +1,8 @@
-# este archivo se definen las rutas de la aplicacion
+from django.urls import path
+from . import views
 
-# ejemplo :
+urlpatterns = [
+    path('', views.index, name='index'),  # Ruta principal
+]
 
-""""
-    from django.urls import path
-    from . import views
-
-    urlpatterns = [
-        path('', views.home, name='home'),
-        path('trainer/', views.trainer, name='trainer'),
-        path('client/', views.client, name='client'),
-    ]
-"""
 
