3b7ee5cfb9c530666c11f7a11171a05a12853bda
alexantonioo <alexcontreras1225@gmail.com>
2024-11-22 23:28:59 -0400

    code restructuring

---
+++ core/urls.py
@@ -0,0 +1,15 @@
+# este archivo se definen las rutas de la aplicacion
+
+# ejemplo :
+
+""""
+    from django.urls import path
+    from . import views
+
+    urlpatterns = [
+        path('', views.home, name='home'),
+        path('trainer/', views.trainer, name='trainer'),
+        path('client/', views.client, name='client'),
+    ]
+"""
+
